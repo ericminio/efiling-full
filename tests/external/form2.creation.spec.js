22a5558f763778db52af631118cbbc67a36276c0
Eric Mignot <eric.mignot@gmail.com>
2019-02-09 16:54:46 -0800

    document form2 creation

---
+++ tests/external/form2.creation.spec.js
@@ -0,0 +1,44 @@
+const { Builder, By } = require('selenium-webdriver');
+const { expect } = require('chai');
+var { execute } = require('yop-postgresql');
+
+describe('Form2 creation', function() {
+
+    var server
+    var driver
+
+    before((done)=> {
+        driver = new Builder().forBrowser('firefox').build()
+        execute('TRUNCATE TABLE forms;', function(rows, error) {
+            expect(error).to.equal(undefined)
+            done();
+        });
+    })
+
+    after(async ()=> {
+        await driver.quit()
+    })
+
+    it('works', async ()=> {
+        await driver.get('http://localhost:3000/form.2.html')
+        let file = await driver.findElement(By.id('file-no'))
+        await file.sendKeys('CA12345')
+        let find = await driver.findElement(By.id('find-button'))
+        await find.click()
+        let appelants = await driver.findElement(By.id('appellant-name'))
+        let value = await appelants.getText()
+
+        expect(value).to.equal('Max FREE, MAX SUPERFREE')
+
+        let phone = await driver.findElement(By.id('phone'))
+        await phone.sendKeys('7783501234')
+        let save = await driver.findElement(By.id('draft'))
+        await save.click()
+
+        await driver.get('http://localhost:3000')
+        await driver.sleep(1000)
+        let cases = await driver.findElements(By.css('table#my-cases tbody tr'))
+
+        expect(cases.length).to.equal(1)
+    })
+})
